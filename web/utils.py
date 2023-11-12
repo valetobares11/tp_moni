660152cd3be145fdc31786d4ef7e35067463595e
valetobares11 <valetobares11@gmail.com>
2023-11-12 20:08:18 -0300

    algunas modificaciones

--- web/utils.py
+++ web/utils.py
@@ -6,20 +6,20 @@ from .serializers import LoansSerializer
 def loan_application_accepted(dni):
     url = settings.URL_MONI.format(dni)
     response = requests.get(url)
-    if (response.json().get('status')=='approve'):
-        return True
-    else:
-        return False
+    
+    return response.json().get('status')=='approve'
 
 def get_fields_loans(loan):
     fields_model_loans = [field.name for field in Loans._meta.get_fields()]
     data = {}
+    
     for field in fields_model_loans:
         data[field] = (getattr(loan, field, None))
-    return data
 
+    return data
 
 def all_loans():
     loans = Loans.objects.all()
     serializer = LoansSerializer(loans, many=True)
+
     return serializer.data
--- web/views.py
+++ web/views.py
@@ -1,19 +1,14 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
 from .models import Users, Loans
 from .forms import LoanForm,UserLoginForm
-
 from .serializers import LoansSerializer, UsersSerializer
 # from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-
 from rest_framework.response import Response
 from rest_framework import status 
 from rest_framework.renderers import TemplateHTMLRenderer
 from .utils import loan_application_accepted, get_fields_loans, all_loans
 from rest_framework import viewsets
-
 
 class LoansModelViewSet(viewsets.ModelViewSet):
     """
@@ -28,36 +23,39 @@ class LoansModelViewSet(viewsets.ModelViewSet):
             queryset = all_loans()
             serializer = self.get_serializer(queryset, many=True)
             return Response({'loans': serializer.data}, template_name='web/loans_list.html')
+        
         loan = self.get_object()
         form = LoanForm(initial=get_fields_loans(loan))
         return render(request, 'web/form_loan_update.html', {'form': form, 'id_loan': loan.id})
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        if serializer.is_valid():
-            data = serializer.validated_data
-            if loan_application_accepted(data['dni']):
-                serializer.save()
-                return redirect('successful_loan')
-            else:
-                return redirect('unsuccessful_loan')
-        return redirect('error')
-
+        if not serializer.is_valid():
+            return redirect('error')
+        
+        data = serializer.validated_data
+        if not loan_application_accepted(data['dni']):
+            return redirect('unsuccessful_loan')
+        
+        serializer.save()
+        return redirect('successful_loan')
+            
     def update(self, request, *args, **kwargs):
         loan = self.get_object()
         serializer = self.get_serializer(loan, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'loans': all_loans()}, template_name='web/loans_list.html')
-        return redirect('error')
-
+        
+        if not serializer.is_valid():
+            return redirect('error')
+        
+        serializer.save()
+        return Response({'loans': all_loans()}, template_name='web/loans_list.html')
+        
     def destroy(self, request, *args, **kwargs):
         loan = self.get_object()
         loan.delete()
+
         return Response({'loans': all_loans()}, template_name='web/loans_list.html')
     
-
-
 
 class UsersModelViewSet(ModelViewSet):
     """
@@ -69,21 +67,25 @@ class UsersModelViewSet(ModelViewSet):
 
     def login(self, request, *args, **kwargs):
         serializer = UsersSerializer(data=request.data)
-        if serializer.is_valid():
-            email = serializer.data.get('email', None)
-            password = serializer.data.get('password', None)
-            user = Users.objects.filter(email=email, password=password).first()
-            if user is not None and user.is_admin:
-                return Response({'loans': all_loans()}, template_name='web/loans_list.html')
-            else:
-                return redirect('not_login')
-        return redirect('error')
+
+        if not serializer.is_valid():
+            return redirect('error')
+
+        email = serializer.data.get('email', None)
+        password = serializer.data.get('password', None)
+        user = Users.objects.filter(email=email, password=password).first()
+        notPerms = user is None or not user.is_admin
+
+        if notPerms:
+            return redirect('not_login')
+
+        return Response({'loans': all_loans()}, template_name='web/loans_list.html')
     
     def list(self, request, *args, **kwargs):
         form = LoanForm()
         formlogin = UserLoginForm()
-        return Response({'form': form, 'formlogin': formlogin}, template_name='web/form_loan.html')
 
+        return Response({'form': form, 'formlogin': formlogin}, template_name='web/form_loan.html')
 
 def error(request):
     """
