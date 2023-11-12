f215cd0e29520d8c416141989ea0a6f395087031
valetobares11 <valetobares11@gmail.com>
2023-11-12 20:27:10 -0300

    correccion de test

--- web/tests.py
+++ web/tests.py
@@ -7,27 +7,27 @@ from .serializers import LoansSerializer
 class LoansTestCase(TestCase):
     def setUp(self):
         Loans.objects.create(id=1,
-                             dni="41018624", 
+                             dni="41018624",
                              name_and_last_name="valentin tobares",
                              email="valetobares11@gmail.com",
                              genre="M",
                              requested_amount=800)
 
-    
     def test_loan_update_dni(self):
         client = Client()
         loan = Loans.objects.filter(id="1").first()
         serializer = LoansSerializer(loan)
         old_dni = serializer.data['dni']
-        data_loan = serializer.data;
-        data_loan['dni'] = '11111111'
-        print(data_loan)
-        response = client.put('/update_loans/1/', data_loan)
+        data_loan = serializer.data
+        new_dni = '11111111'
+        data_loan['dni'] = new_dni
+        headers = {'Content-Type': 'application/json'}
+        response = client.put('/update_loans/1/', data=json.dumps(data_loan), headers=headers)
         print(response)
-        # self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
         loan = Loans.objects.filter(id="1").first()
         print(loan)
-        self.assertTrue(loan.dni != old_dni and loan.dni=='11111111')
+        self.assertTrue(loan.dni != old_dni and loan.dni==new_dni)
 
     def test_loan_update_email(self):
         client = Client()
@@ -35,63 +35,61 @@ class LoansTestCase(TestCase):
         serializer = LoansSerializer(loan)
         email_old = serializer.data['email']
         data_loan = serializer.data
-        data_loan['email'] = 'valetobares@gmail.com'
-        response = client.put('/update_loans/1/', data_loan)
-        # self.assertEqual(response.status_code, 200)
+        new_email = 'valetobares@gmail.com'
+        data_loan['email'] = new_email
+        headers = {'Content-Type': 'application/json'}
+        response = client.put('/update_loans/1/', data=json.dumps(data_loan), headers=headers)
+        self.assertEqual(response.status_code, 200)
         loan = Loans.objects.filter(id="1").first()
-        self.assertFalse(loan.email == email_old)
+        self.assertTrue(loan.email != email_old and loan.email == new_email)
 
-    # def test_loan_update_error_incomplete_form(self):
-    #     client = Client()
-    #     loan = Loans.objects.filter(id="1").first()
-    #     loan_data = {
-    #         'id':1,
-    #         'dni':'11111111'
-    #     }
-    #     response = client.post('/update_loans/1/', loan_data)
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertTrue(response.url=="/error/")
+    def test_loan_update_error_incomplete_form(self):
+        client = Client()
+        loan = Loans.objects.filter(id="1").first()
+        loan_data = {
+            'id':1,
+            'dni':'11111111'
+        }
+        headers = {'Content-Type': 'application/json'}
+        response = client.put('/update_loans/1/', data=json.dumps(loan_data), headers=headers)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url=="/error/")
 
+    def test_loan_update_error_id_not_exist(self):
+        client = Client()
+        loan_data = {
+            'id':1,
+            'dni':'11111111',
+            'name_and_last_name':'valentin tobares',
+            'email':'valetobares11@gmail.com',
+            'genre':'M',
+            'requested_amount':'8000'
+        }
+        response = client.put('/update_loans/99/', loan_data)
+        self.assertEqual(response.status_code, 404)
 
-    # def test_loan_update_error_id_not_exist(self):
-    #     client = Client()
-    #     loan_data = {
-    #         'id':1,
-    #         'dni':'11111111',
-    #         'name_and_last_name':'valentin tobares',
-    #         'email':'valetobares11@gmail.com',
-    #         'genre':'M',
-    #         'requested_amount':'8000'
-    #     }
-    #     response = client.post('/update_loans/99/', loan_data)
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertTrue(response.url=="/error/")
+    def test_index_error_incomplete_form(self):
+        client = Client()
+        loan_data = {
+            'dni':'11111111',
+            'name_and_last_name':'valentin tobares'
+        }
+        headers = {'Content-Type': 'application/json'}
+        response = client.put('/update_loans/1/', data=json.dumps(loan_data), headers=headers)
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(response.url=="/error/")
 
-    # def test_index_error_incomplete_form(self):
-    #     client = Client()
-    #     loan_data = {
-    #         'dni':'11111111',
-    #         'name_and_last_name':'valentin tobares'
-    #     }
-    #     response = client.post('/update_loans/1/', loan_data)
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertTrue(response.url=="/error/")
+    def test_loan_delete(self):
+        client = Client()
+        response = client.delete('/delete_loans/1/')
+        self.assertEqual(response.status_code, 200)
+        loan = Loans.objects.filter(id=1).first()
+        self.assertEquals(loan, None)
 
-  
-
-    # def test_loan_delete(self):
-    #     client = Client()
-    #     response = client.delete('/delete_loans/1/')
-    #     self.assertEqual(response.status_code, 200)
-    #     loan = Loans.objects.filter(id=1).first()
-    #     self.assertEquals(loan, None)
-
-
-    # def test_loan_delete_not_found(self):
-    #     client = Client()
-    #     response = client.delete('/delete_loans/99/')
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertTrue(response.url=="/error/")
+    def test_loan_delete_not_found(self):
+        client = Client()
+        response = client.delete('/delete_loans/99/')
+        self.assertEqual(response.status_code, 404)
 
 
 class UsersTestCase(TestCase):
